e55d9a3bd272e38aef193a235f68be591a749441
pmenezes85 <paulo.menezes@iteris.com.br>
2020-01-15 17:01:46 -0300

    Finishing api requests

--- src/App.js
+++ src/App.js
@@ -1,22 +1,141 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React, { useEffect, useState } from 'react';
+import api from './services/api';
 
-// Componente
-// Estado
-// Propriedade
+import './global.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
 
-  function incrementCounter() {
-    setCounter(counter + 1);
+  const [devs, setDevs] = useState([]);
+
+  const [github_username, setGithubUsername] = useState('');
+  const [techs, setTechs] = useState('');
+
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+
+        setLatitude(latitude);
+        setLongitude(longitude);
+      }, (err) => {
+        console.log(err);
+      },
+      {
+        timeout: 30000
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get('/devs');
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(e) {
+    e.preventDefault();
+
+    const response = await api.post('/devs', {
+      github_username,
+      techs,
+      latitude,
+      longitude
+    });
+
+    setGithubUsername('');
+    setTechs('');
+
+    setDevs([...devs, response.data])
   }
 
   return (
-    <>
-      <h1>Contador: {counter}</h1>
-      <button onClick={incrementCounter}>Incrementar</button>
-    </>
+    <div id="app">
+
+      <aside>
+        <strong>Cadastrar</strong>
+        <form onSubmit={handleAddDev}>
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do Github</label>
+            <input 
+              name="github_username" 
+              id="username_github" 
+              required
+              value={github_username}
+              onChange={e => setGithubUsername(e.target.value)} 
+            />
+          </div>
+          
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input 
+              name="techs" 
+              id="techs" 
+              required
+              value={techs}
+              onChange={e => setTechs(e.target.value)} 
+            />
+          </div>
+          
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input 
+                type="number" 
+                name="latitude" 
+                id="latitude" 
+                required 
+                value={latitude}
+                onChange={e => setLatitude(e.target.value)}
+              />
+            </div>
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input 
+                type="number" 
+                name="longitude" 
+                id="longitude" 
+                required 
+                value={longitude}
+                onChange={e => setLongitude(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <button type="submit">Salvar</button>
+          
+        </form>
+      </aside>
+
+      <main>
+
+        <ul>
+          {devs.map(dev => (
+            <li key={dev._id} className="dev-item">
+            <header>
+              <img src={dev.avatar_url} alt={dev.name}/>
+              <div className="user-info">
+                <strong>{dev.name ? dev.name : 'Unknown'}</strong>
+                <span>{dev.techs.join(', ')}</span>
+              </div>
+            </header>
+            <p>{dev.bio ? dev.bio : 'No bio available'}</p>
+            <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no Github</a>
+          </li>
+          ))}
+        </ul>
+      </main>
+
+    </div>
   );
 }
 
